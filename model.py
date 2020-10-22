697b40e82fce04a4cd5da90868c4a1012c2280e5
UTKARSH TRIPATHI <65115042+ukarsh-tripathi@users.noreply.github.com>
2020-10-22 11:57:29 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
+df = pd.read_csv('heart.csv') 
+from sklearn.model_selection import train_test_split
+X = df.drop('target', axis = 1).values
+y = df['target'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+lr = LogisticRegression()
+lr.fit(X_train,y_train)
+pickle.dump(lr,open('model.pkl','wb'))
+heart=pickle.load(open('model.pkl','rb'))
+print(heart.predict([[63,1,3,145,233,1,0,150,0,2.3,0,0,1]]))
